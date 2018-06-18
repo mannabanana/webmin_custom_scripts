125c69849ea6a871ef3b7a7ee7487469b44ed3e7
mannabanana <40088708+mannabanana@users.noreply.github.com>
2018-06-18 11:12:49 +0300

    Add files via upload
    Blocking Internet address in squid for domain users via Webmin

--- blocksite_via_squid.bash
+++ blocksite_via_squid.bash
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-PC=${NAME::${#NAME}-3}
-if [ $PC = dt ] || [ $PC = ws ] || [ $PC = xz ] || [ $PC = pc ]
-then
-NUM=${NAME:2}
-if [ $NUM -gt 100 ] && [ $NUM -lt 256 ]
-then
-#attempts to find user's pc name
-if egrep '*\<'$NAME'\>.*' /etc/squid/squid.conf > /dev/null;
-then
-sed -i '/'$NAME'/s/#acl/acl/g' /etc/squid/squid.conf
-sed -i '/'$NAME'/s/#http_access/http_access/g' /etc/squid/squid.conf
-#looking for a point '.' at the begin of address. if it's absent the script would add '.'
+#проверяет есть ли точка перед ссылкой, если нет, то добавляет
+URL=${URL,,}
 if [ ${URL:0:1} != . ]
 then
 URL='.'$URL
 fi
 
 #attempts to check a blocking address. The full domain name is not a good idea
-TLD3=${URL:${#URL}-4}
-TLD2=${URL:${#URL}-3}
-if [ $TLD3 = .com ] || [ $TLD3 = .net ] || [ $TLD3 = .org ] || [ $TLD3 = .gov ] && [ ${#URL} -eq ${#TLD3} ]
+TLD=${URL##*.}
+if grep -e '^'$TLD'\>' /etc/webmin/scripts/dns_name.txt > /dev/null
 then
-echo "Incorrect '$URL'"
+if [ ${#URL} -eq ${#TLD} ]
+then
+echo "To block all domain is not good"
 exit 0
-elif [ $TLD2 = .ru ] && [ ${#URL} -eq ${#TLD2} ]
-then
+fi
+else
 echo "Incorrect '$URL'"
 exit 0
 fi
+
+NAME=${NAME,,}
+PC=${NAME::${#NAME}-3}
+if [ $PC = dt ] || [ $PC = ws ] || [ $PC = zx ] || [ $PC = pc ]
+then
+NUM=${NAME:2}
+if [ $NUM -gt 100 ] && [ $NUM -lt 256 ]
+then
+if egrep '*\<'$NAME'\>.*' /etc/squid/squid.conf > /dev/null;
+then
+sed -i '/'$NAME'/s/#acl/acl/g' /etc/squid/squid.conf
+sed -i '/'$NAME'/s/#http_access/http_access/g' /etc/squid/squid.conf
 
 #find the user's PC file
 if [ -f /etc/squid/block_pc/block_$PC/$NAME.txt ]
@@ -37,28 +40,27 @@ then
 if egrep '^#'$URL'$' /etc/squid/block_pc/block_$PC/$NAME.txt
 then
 sed -i 's/#'$URL'/'$URL'/g' /etc/squid/block_pc/block_$PC/$NAME.txt
+echo "All list of blocking sites"
 cat /etc/squid/block_pc/block_$PC/$NAME.txt
 /bin/systemctl restart squid.service
-#if the address have been blocked yet
+#if the address haven't been blocked
 elif egrep '^'$URL'$' /etc/squid/block_pc/block_$PC/$NAME.txt
 then
 echo "'$URL' exist"
+echo "All list of blocking sites"
 cat /etc/squid/block_pc/block_$PC/$NAME.txt
 else
-#if the address haven't been blocked
+#if the file of blocking doesn't found
 echo -e "$URL" >> /etc/squid/block_pc/block_$PC/$NAME.txt
 cat /etc/squid/block_pc/block_$PC/$NAME.txt
 /bin/systemctl restart squid.service
 fi
-#if the file of blocking doesn't found
 else
 touch /etc/squid/block_pc/block_$PC/$NAME.txt
 echo -e "$URL" >> /etc/squid/block_pc/block_$PC/$NAME.txt
 /bin/systemctl restart squid.service
 fi
-#else
-#echo "Incorrect '$URL'"
-#fi
+
 else
 echo "'$NAME' doesn't exist"
 fi
